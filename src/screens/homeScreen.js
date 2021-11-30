4219ab8b8905b33ce2cf8dd85b730aa58eb07c06
gEstefania <egarcia@solucionesroots.com>
2021-11-30 07:54:41 -0600

    Add Top Tab Navigation Screens

---
+++ src/screens/homeScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const homeScreen = () => {
+    return (
+        <View>
+            <Text>Home Screen</Text>
+        </View>
+    )
+}
+
+export default homeScreen;
