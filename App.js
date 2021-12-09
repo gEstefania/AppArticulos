f6fb4520ecb9780ea2005ba72b13acf83d6c9aae
gEstefania <egarcia@solucionesroots.com>
2021-12-09 10:43:29 -0600

    Integrate Tab and Stack navigator

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
+import 'react-native-gesture-handler';
 import * as React from 'react';
 import AppNavigator from './src/navigation/appNavigator'
 
 export default function App() {
   return (
-      <AppNavigator/>
+    <AppNavigator/>
   );
 }
--- src/navigation/appNavigator.js
+++ src/navigation/appNavigator.js
@@ -1,33 +1,65 @@
 import * as React from 'react';
+import { Image, View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import exploreScreen from '../screens/exploreScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import LinearGradient from 'react-native-linear-gradient';
+import exploreScreen from '../screens/Explore/exploreScreen';
 import infoScreen from '../screens/Information/index';
 import trainingScreen from '../screens/trainingScreen';
-import { Image, View } from 'react-native';
+import topicScreen from '../screens/Information/screens/Topic/topicScreen';
+import articleScreen from '../screens/Information/screens/Topic/articleScreen';
+import stepScreen from '../screens/Information/screens/Topic/stepScreen';
+import postScreen from '../screens/Information/screens/Topic/postScreen';
+
+const InformationStack = createStackNavigator();
+
+function InformationStackScreen() {
+  return (
+    <InformationStack.Navigator
+    screenOptions={{
+      headerTitle: "",
+      headerBackTitleVisible: false,
+      headerStyle: {shadowColor: '#fff'},
+    }}
+    >
+      <InformationStack.Screen name="Information" component={infoScreen} options={{headerShown: false}}/>
+      <InformationStack.Screen name="Topic" component={topicScreen} />
+      <InformationStack.Screen name="Article" component={articleScreen} />
+      <InformationStack.Screen name="Step" component={stepScreen} />
+      <InformationStack.Screen name="Post" component={postScreen} />
+    </InformationStack.Navigator>
+  );
+}
 
 const AppNavigator = () => {
-  
   const Tab = createMaterialTopTabNavigator();
-
-    return (
-      <NavigationContainer>
-        <View style={{backgroundColor: '#ff9b04', height: 125, justifyContent: 'flex-end', alignItems: 'center'}}>
+  return (
+    <NavigationContainer>
+      <LinearGradient
+        colors={['#ff9b04', '#ff000a' ]}
+        style={{flex: 1}}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        locations={[0.2, 0.4]}
+      >
+        <View style={{backgroundColor: 'transparent', height: 125, justifyContent: 'flex-end', alignItems: 'center'}}>
           <Image style={{width: 250, height: 70}} source={require('../assets/img/logo.png')}/>
         </View>
         <Tab.Navigator
-        screenOptions={{
-          tabBarLabelStyle: { fontWeight: '600', fontFamily: 'Poppins-Bold', textTransform: 'capitalize', color: '#fff', fontSize: 16},
-          tabBarStyle: { backgroundColor: '#ff9b04', paddingVertical: 15, },
-          tabBarIndicatorStyle: {backgroundColor: '#DCD5D3', height: 7,},
-        }}
+          screenOptions={{
+            tabBarLabelStyle: { fontWeight: '600', fontFamily: 'Poppins-Bold', textTransform: 'capitalize', color: '#fff', fontSize: 16},
+            tabBarStyle: { backgroundColor: 'transparent', paddingVertical: 15, },
+            tabBarIndicatorStyle: {backgroundColor: 'silver', height: 7,},
+          }}
         >
           <Tab.Screen name="Explorar" component={exploreScreen} />
-          <Tab.Screen name="Información" component={infoScreen} />
+          <Tab.Screen name="Información" component={InformationStackScreen} />
           <Tab.Screen name="Formación" component={trainingScreen} />
       </Tab.Navigator>
-      </NavigationContainer>
-    );
+    </LinearGradient>
+    </NavigationContainer>
+  );
 }
 
 export default AppNavigator;
