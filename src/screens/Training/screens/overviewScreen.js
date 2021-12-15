c454f47eff19386dd1d9b90c111f7466a9bea093
gEstefania <egarcia@solucionesroots.com>
2021-12-15 12:01:05 -0600

    Overview Screen and its style

--- src/screens/Training/screens/overviewScreen.js
+++ src/screens/Training/screens/overviewScreen.js
@@ -1,10 +1,34 @@
 import * as React from 'react';
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import styles from './styles/overviewScreen';
 
 const overviewScreen = () => {
     return (
-        <View>
-            <Text>Resumen del curso</Text>
+        <View style={styles.mainContainer}>
+            <View style={styles.summaryContainer}>
+                <View style={styles.row}>
+                    <Image source={require('../../../assets/img/icons/home.jpg')} style={styles.icon}/>
+                    <View style={styles.columnText}>
+                        <Text style={styles.infoText}>6 videos</Text>
+                    </View>
+                </View>
+                <View style={styles.row}>
+                    <Image source={require('../../../assets/img/icons/home.jpg')} style={styles.icon}/>
+                    <View style={styles.columnText}>
+                        <Text style={styles.infoText}>40 minutos</Text>
+                    </View>
+                </View>
+                <View style={styles.row}>
+                    <Image source={require('../../../assets/img/icons/home.jpg')} style={styles.icon}/>
+                    <View style={styles.columnText}>
+                        <Text style={styles.infoText}>2,122 inscritos</Text>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.descContainer}>
+                <Text>Aprenderemos a</Text>
+                <Text>autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</Text>
+            </View>
         </View>
     )
 }
---
+++ src/screens/Training/screens/styles/overviewScreen.js
@@ -0,0 +1,30 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    marginHorizontal: 20,
+  },
+  summaryContainer:{
+    flex: 1,
+    backgroundColor: 'rgba(255, 155, 4, 0.5)', //se va a sustituir por una imagen.
+    marginBottom: 30,
+    padding: 30,
+    borderRadius: 30,
+    justifyContent: 'space-between'
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  columnText:{
+    marginLeft: 10,
+    justifyContent: 'center',
+  },
+  infoText:{
+    color: '#fff',
+    fontFamily: 'Poppins-Bold',
+  },
+  descContainer: {
+    flex: 2,
+  },
+});
