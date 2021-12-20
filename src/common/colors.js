f3750e3b743fdc1c80a855489b3708056ecda3cc
gEstefania <egarcia@solucionesroots.com>
2021-12-20 16:09:53 -0600

    Create common elements

---
+++ src/common/colors.js
@@ -0,0 +1,8 @@
+export default {
+    CORPORATE_ORANGE: '#ff9b04',
+    PINK: '#e55773',
+    ORANGE: '#ff5f00',
+    SKY_BLUE: '#11b2d8',
+    PURPLE: '#5f40d5',
+    SECONDARY_ORANGE: '#ffe49b',
+  };
---
+++ src/common/secondaryText.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+const SecondaryText = (props) => {
+  const styles = StyleSheet.create({
+  font: {
+      fontFamily: `Poppins-${props.type ? props.type : 'Regular'}`,
+    },
+  });
+  return (
+    <Text
+      onPress={props.onPress}
+      numberOfLines={props.numberOfLines}
+      onTextLayout={props.onTextLayout}
+      style={[props.style, styles.font]}
+      //ellipsizeMode="tail"
+    >
+      {props.children}
+    </Text>
+  );
+};
+
+export default SecondaryText;
